49e005ef5f10779928832ed348361ef52566f5eb
Rivera <J.C.RIVERA@cgiar.org>
2018-02-26 11:45:32 -0500

    jcr

--- Graphics.R
+++ Graphics.R
@@ -19,14 +19,31 @@ list_files <- lapply(list.files(here::here('Data'),full.names = T), function (x)
 
 
 #Graph_station plots variable each station 
-#Arguments  - name_station  = name of station 
+#Arguments  - name_station  = name of station e.g. (21255160, MixIbague, Piedra) 
+#                           
 #           - variables ESOL = Solar Irradiation
 #                       RAIN = Precipitation
 #                       RHUM = Relative Humidity
 #                       TMAX = Maximun Temperature
 #                       TMIN = Minimun Temperature
+#           - period         = period of graphic
 #Return    - graph of station 
 
-Graph_station
+Graph_station <- function (name_station, variable, period=NULL)
+{
+  #Read file
+  file <- read.table(list.files(here::here('Data'),full.names = T, pattern = paste0(name_station, "_", variable)), header=T)
+  
+  #Format
+  file$Dates <- as.Date(as.character(file$Dates), format = "%Y-%m-%d")
+  file$Value <- as.numeric(file$Value)
+  
+  #Plot
+  
+  
+  return (file)
+  
+  
+}
 
 
