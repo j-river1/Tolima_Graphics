bc65fd82225d6d40fc91757da4d7ba32e92f6ae9
Rivera <J.C.RIVERA@cgiar.org>
2018-02-26 14:07:16 -0500

    jcr

--- Graphics.R
+++ Graphics.R
@@ -31,6 +31,41 @@ list_files <- lapply(list.files(here::here('Data'),full.names = T), function (x)
 
 Graph_station <- function (name_station, variable, period=NULL)
 {
+  
+  if(variable == "ESOL")
+  {
+    y <- "calorias_cm2_diarios"
+    title <- "Radiación Solar"
+  }
+  
+  if(variable == "RAIN")
+  {
+    y <- "Mililitros"
+    title <- "Precipitación"
+  }
+  
+  if(variable == "RHUM")
+  {
+    y <- "Valor"
+    title <- "Humedad Relativa"
+  }
+  
+  if(variable == "TMAX")
+  {
+    y <- "Grados_Centigrados"
+    title <- "Temperatura Mínima"
+  }
+  
+  
+  if(variable == "TMIN")
+  {
+    y <- "Grados_Centigrados"
+    title <- "Temperatura Máxima"
+  }
+  
+  x <- "Dias"
+  
+  
   #Read file
   file <- read.table(list.files(here::here('Data'),full.names = T, pattern = paste0(name_station, "_", variable)), header=T)
   
@@ -38,9 +73,15 @@ Graph_station <- function (name_station, variable, period=NULL)
   file$Dates <- as.Date(as.character(file$Dates), format = "%Y-%m-%d")
   file$Value <- as.numeric(file$Value)
   
+  #Data
+  colnames(file)[3] <- c("Data")
+  levels(file$Data) <- c("Datos_Reales", "Datos_Estimados")
+  
+  
   #Plot
-  
-  
+  ggplot(file, aes(x=Dates, y=Value, colour= Data)) + geom_point() + labs(y = y, x = x ) +
+  ggtitle(paste0("Estación ", name_station, "\n", title))+ theme(plot.title = element_text(hjust = 0.5))
+  ggsave(paste0("./Graphics/",name_station, "_", variable, ".jpg"))
   return (file)
   
   
